1280f8edd87e70f50d6b5f740668d836d7032ec1
park jong ik <pji219@naver.com>
2020-12-24 23:59:46 +0900

    1

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { Provider} from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(Reducer, // reducer 연결
+      window.__REDUX_DEVTOOLS_EXTENSION__ && // 크롬 확장 Redux Dev Tool 연결
+      window.__REDUX_DEVTOOLS_EXTENSION__() 
+    )}
+  >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
