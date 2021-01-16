e46a36a6932c4c3a037a15ffbb30b46fc3457f56
park jong ik <pji219@naver.com>
2021-01-16 22:59:02 +0900

    리덕스로 회원가입 작업

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,5 +1,6 @@
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from '../_actions/types';
 
 // 파라미터 state는 이전 state
@@ -7,6 +8,9 @@ export default function (state = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
+            break;
+        case REGISTER_USER:
+            return { ...state, register: action.payload }
             break;
 
         default:
